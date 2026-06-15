54a0399bce725882ffd714cb51d9c1d892fcf2da
Mat Jones <mat@mjones.network>
2026-06-15 12:23:25 -0400

    fix(cd): Update `release-plz-patched` pkg
    Fixes some edge cases around nonlinear history.

--- nix/pkgs/release-plz-patched.nix
+++ nix/pkgs/release-plz-patched.nix
@@ -4,8 +4,8 @@ pkgs.release-plz.overrideAttrs (old: {
   patches = (old.patches or [ ]) ++ [
     (pkgs.fetchpatch {
       name = "walk-all-branches-2857.patch";
-      url = "https://github.com/release-plz/release-plz/commit/0bf9940e70958cb3777dae063bfda2db77d40502.patch";
-      hash = "sha256-ii1SQ64Wg38sbboafTReOJji8brBYOBfaDjKHbe7SoI=";
+      url = "https://github.com/release-plz/release-plz/commit/2dbce2513eea25920c0e37826d3c4eb38e25dcd4.patch";
+      hash = "sha256-jcY7luLkI4YCLM93KhL1vaKkFMdPnGMzBEePJ+cHjkc=";
     })
   ];
 })
